2335fc50f061a357e858c317cd4198d57c7e32ef
Janmejay Punetha <janmejaypunetha2@gmail.com>
2023-06-21 17:21:40 +0530

    Second com wed

---
+++ IncrementalSieve.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class IncrementalSieve {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the upper bound: ");
+        int n = sc.nextInt();
+
+        boolean[] primes = new boolean[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            primes[i] = true;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (primes[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    primes[j] = false;
+                }
+            }
+        }
+
+        // Print the prime numbers
+        System.out.println("The prime numbers are:");
+        for (int i = 2; i <= n; i++) {
+            if (primes[i]) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+}

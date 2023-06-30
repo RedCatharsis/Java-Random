cc9cad26364f5990d78d0976f4957e67208e342f
Janmejay Punetha <janmejaypunetha2@gmail.com>
2023-06-30 14:09:05 +0530

    save

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -9,14 +9,17 @@ public class PrimeNumbers {
 
     int primeCount = 0;
     int nonPrimeCount = 0;
+
     for (int i = 2; i <= upper_bound; i++) {
       boolean isPrime = true;
+
       for (int j = 2; j * j <= i; j++) {
         if (i % j == 0) {
           isPrime = false;
           break;
         }
       }
+      
       if (isPrime) {
         primeCount++;
         System.out.println("Prime number: " + i);
